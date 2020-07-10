f75b3e9bf910254dc580e4699a25d72e52cc19c3
Anuradha Aggarwal <anuradha.mcs18.du@gmail.com>
2020-07-10 23:06:34 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from "react";
 import "./App.css";
 import Button from "./components/Button.jsx";
 import Footer from "./components/Footer.jsx";
+import Header from './components/Header.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
+
 
 function App() {
   return (
     <div className="App">
+      <Header width="30" height="30" logoImg={logo} heading="funkids"></Header>
+
       <Button
         onClick={() => console.log("Button clicked!!")}
         type="button"
         buttonStyle="btn--primary--outline"
         buttonSize="btn--small"
       ></Button>
+
       <Footer
         year="2020"
         footerColor="footer--danger--outline"
